7672ae8c02661f94b7981ae8167e7e421930979a
Gregory Petukhov <lorien@lorien.name>
2015-01-26 13:11:26 +0700

    Add meta option/attribute to Task object

--- iob/request.py
+++ iob/request.py
@@ -1,5 +1,6 @@
 class Request(object):
-    def __init__(self, url, callback=None, tag=None):
+    def __init__(self, url, callback=None, tag=None, meta=None):
         self.url = url
         self.callback = callback
         self.tag = tag
+        self.meta = meta
