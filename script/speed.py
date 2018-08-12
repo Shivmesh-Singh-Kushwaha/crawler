4a0f91ed23eb55a04588bd53d5c0d5b7d4e0b326
Gregory Petukhov <lorien@lorien.name>
2018-08-12 13:50:14 +0300

    Change script/speed.py

--- script/speed.py
+++ script/speed.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 from crawler import Crawler, Request
 
@@ -6,17 +7,19 @@ from crawler import Crawler, Request
 class TestCrawler(Crawler):
     def task_generator(self):
         for x in range(self._meta['num_req']):
-            yield Request('page', 'http://127.0.0.1/test.txt')
+            yield Request('page', 'http://127.0.0.1/awesome_python.html')
 
     def init_hook(self):
         self.count = 0
 
-    def handler_page(self, req, resp):
-        assert resp.body.strip() == b'test'
+    def handler_page(self, req, res):
+        title = res.xpath('//title').text()
+        assert title.startswith('awesome')
         self.count += 1
 
 
 def main(**kwargs):
+    logging.getLogger('crawler.network').propagate = False
     start = time.time()
     num_req = 1000
     bot = TestCrawler(num_network_threads=10, meta={'num_req': num_req})
